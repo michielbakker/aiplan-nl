9146c27bb1f920d42963c725252aca3967259310
Stan van Baarsen <stanvanbaarsen@hotmail.com>
2025-10-01 11:11:52 +0100

    Update Vite config to include base path for asset referencing, to fix google translate API CORS problems

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,20 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  assetsInclude: ['**/*.md'],
+	server: {
+		host: "::",
+		port: 8080,
+	},
+	plugins: [
+		react(),
+		mode === 'development' &&
+		componentTagger(),
+	].filter(Boolean),
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	assetsInclude: ['**/*.md'],
+	base: "./",
 }));
